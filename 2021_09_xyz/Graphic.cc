b598fd689d0cfda96eac50c97bd970de9a3f12a2
app-pro <app-pro@app-pro-pc>
2022-04-01 02:44:48 +0500

    Adding "img_init", "img_reset_horizon", "img_draw_pixel", "img_save_tga" and "img_save_bmp" voids

--- 2021_09_xyz/Graphic.cc
+++ 2021_09_xyz/Graphic.cc
@@ -25,6 +25,12 @@ constexpr uint32_t COL_WHITE = 0xffffffff;
 
 #pragma pack(push, 1)
 
+struct Image {
+	std::vector<uint32_t> pixels;
+	std::vector<int> horizon;
+	int width;
+	int height;
+};
 struct TGAHeader {
 	uint8_t id_len; //длина идентификатора
 	uint8_t pal_type; //тип палитры
@@ -78,13 +84,13 @@ void fill_rect(std::vector<uint32_t> &image, int x0, int y0, int w, int h,
 			image[y * IMG_WIDTH + x] = color;
 }
 
-void draw_pixel(std::vector<uint32_t> &image, int x, int y, uint32_t color) {
+/*void draw_pixel(std::vector<uint32_t> &image, int x, int y, uint32_t color) {
 	if (x < 0 or x >= IMG_WIDTH or y < 0 or y >= IMG_HEIGHT)
 		return;
 	image[y * IMG_WIDTH + x] = color;
-}
+}*/
 
-void draw_pixel_2(std::vector<uint32_t> &image, std::vector<int> &horizon,
+/*void draw_pixel_2(std::vector<uint32_t> &image, std::vector<int> &horizon,
 		int x, int y, uint32_t color) {
 	if (x < 0 or x >= IMG_WIDTH)
 		return;
@@ -94,7 +100,7 @@ void draw_pixel_2(std::vector<uint32_t> &image, std::vector<int> &horizon,
 	if (y >= IMG_HEIGHT)
 		return;
 	image[y * IMG_WIDTH + x] = color;
-}
+}*/
 
 void convert_isometry(double x, double y, double z, int &px, int &py) {
 	px = int(
@@ -107,77 +113,98 @@ void convert_isometry(double x, double y, double z, int &px, int &py) {
 			YSCALE * sin(Pi / 6.0) * y - ZSCALE * z);
 }
 
-int main() {
-	std::vector<uint32_t> image;
-	std::vector<int> horizon;
-	image.resize(IMG_WIDTH * IMG_HEIGHT);
-	horizon.resize(IMG_WIDTH);
+void img_init(Image &image, int width, int height, uint32_t background) {
+	image.width = width;
+	image.height = height;
+	image.pixels.resize(width * height);
+	image.horizon.resize(width);
 
-	//покраска фона
-	for (auto &&p : image)
-		p = COL_BLACK;
+	for (auto &&p : image.pixels)
+		p = background;
+}
 
-	//fill_rect(image, 64, 64, 96, 96, 0xffffff00);
-	//fill_rect(image, 80, 80, 96, 96, 0xffff00ff);
-	//fill_rect(image, 128, 128, 96, 96, 0xff00ffff);
-
-	double y, x, z;
-	int px, py;
-	for (auto &&y : horizon)
+void img_reset_horizon(Image &image) {
+	for (auto &&y : image.horizon)
 		y = IMG_HEIGHT;
+}
+
+void img_draw_pixel(Image &image, int x, int y, uint32_t color) {
+	if (x < 0 or x >= image.width)
+		return;
+	if (y >= image.horizon[x])
+		return;
+	image.horizon[x] = y;
+	if (y >= image.height)
+		return;
+	image.pixels[y * image.width + x] = color;
+}
+
+void img_save_tga(const Image &image, const char *filename) {
+	TGAHeader header { };
+	std::ofstream tga_file { filename, std::ios::binary };
+	header.img_type = 2;
+	header.width = image.width;
+	header.height = image.height;
+	header.depth = 32;
+	header.img_desc = 0b00101000;
+	tga_file.write(reinterpret_cast<const char*>(&header), sizeof(header));
+	tga_file.write(reinterpret_cast<const char*>(&image.pixels[0]),
+			image.pixels.size() * sizeof(image.pixels[0]));
+	tga_file.close();
+}
+
+void img_save_bmp(const Image &image, const char *filename) {
+	BMPFileHeader bmpf { };
+	BMPInfoHeader bmpi { };
+	std::ofstream bmp_file { filename, std::ios::binary };
+
+	bmpi.header_length = sizeof(bmpi);
+	bmpi.width = image.width;
+	bmpi.height = -image.height;
+	bmpi.depth = 32;
+	bmpi.img_length = image.pixels.size() * sizeof(image.pixels[0]);
+	bmpi.planes = 1;
+
+	bmpf.signature = 0x0001 * 'B' + 0x0100 * 'M';
+	bmpf.img_offset = sizeof(bmpf) + sizeof(bmpi);
+	bmpf.file_size = bmpf.img_offset + bmpi.img_length;
+
+	bmp_file.write(reinterpret_cast<const char*>(&bmpf), sizeof(bmpf));
+	bmp_file.write(reinterpret_cast<const char*>(&bmpi), sizeof(bmpi));
+	bmp_file.write(reinterpret_cast<const char*>(&image.pixels[0]), bmpi.img_length);
+	bmp_file.close();
+}
+
+int main() {
+	//создание экземпл€ра структуры
+	Image image;
+	//заполнение параметров экземпл€ра
+	img_init(image, IMG_WIDTH, IMG_HEIGHT, COL_BLACK);
+
+	double x, y, z;
+	int px, py;
+
+	img_reset_horizon(image);
 	//линии рисуютс€ параллельно XZ
 	for (y = YMAX; y >= YMIN; y -= BIGSTEP)
 		for (x = XMAX; x >= XMIN; x -= SMALLSTEP) {
 			z = my_func(x, y);
 			convert_isometry(x, y, z, px, py);
-			draw_pixel_2(image, horizon, px, py, COL_WHITE);
+			img_draw_pixel(image, px, py, COL_WHITE);
 		}
-	for (auto &&y : horizon)
-			y = IMG_HEIGHT;
+
+	img_reset_horizon(image);
 	//линии рисуютс€ пареллельно YZ
 	for (x = XMAX; x >= XMIN; x -= BIGSTEP)
 		for (y = YMAX; y >= YMIN; y -= SMALLSTEP) {
 			z = my_func(x, y);
 			convert_isometry(x, y, z, px, py);
-			draw_pixel_2(image, horizon, px, py, COL_WHITE);
+			img_draw_pixel(image, px, py, COL_WHITE);
 		}
 
-	TGAHeader header { };
-	std::ofstream tga_file { "output.tga", std::ios::binary };
-	header.img_type = 2;
-	header.width = IMG_WIDTH;
-	header.height = IMG_HEIGHT;
-	header.depth = 32;
-	header.img_desc = 0b00101000;
+	img_save_tga(image, "output.tga");
 
-	if (not tga_file) {
-		std::cerr << "Error" << strerror(errno) << std::endl;
-		return 1;
-	}
-	tga_file.write(reinterpret_cast<char*>(&header), sizeof(header));
-	tga_file.write(reinterpret_cast<char*>(&image[0]),
-			image.size() * sizeof(image[0]));
-	tga_file.close();
-
-	BMPFileHeader bmpf { };
-	BMPInfoHeader bmpi { };
-	std::ofstream bmp_file { "output.bmp", std::ios::binary };
-	bmpf.signature = 0x0001 * 'B' + 0x0100 * 'M';
-	bmpf.img_offset = sizeof(bmpf) + sizeof(bmpi);
-	bmpf.file_size = bmpf.img_offset + image.size() * sizeof(image[0]);
-
-	bmpi.header_length = sizeof(bmpi);
-	bmpi.width = IMG_WIDTH;
-	bmpi.height = -IMG_HEIGHT;//?
-	bmpi.depth = 32;
-	bmpi.img_length = image.size() * sizeof(image[0]);
-	bmpi.planes = 1;
-
-	bmp_file.write(reinterpret_cast<char*>(&bmpf), sizeof(bmpf));
-	bmp_file.write(reinterpret_cast<char*>(&bmpi), sizeof(bmpi));
-	bmp_file.write(reinterpret_cast<char*>(&image[0]),
-			image.size() * sizeof(image[0]));
-	bmp_file.close();
+	img_save_bmp(image, "output.bmp");
 
 	return 0;
 
