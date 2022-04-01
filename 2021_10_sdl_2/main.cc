9d6f796626cb9ed8da49d6c17fc2727601fed3c9
app-pro <app-pro@app-pro-pc>
2022-04-01 22:27:23 +0500

    Adding "create_window", "create_renderer" and "render" voids

--- 2021_10_sdl_2/main.cc
+++ 2021_10_sdl_2/main.cc
@@ -26,55 +26,66 @@
 const int WINDOW_HEIGHT = 600;
 const int WINDOW_WIDTH = 800;
 
-int main(int, char**) {
-	SDL_Init(SDL_INIT_EVERYTHING);
+std::shared_ptr<SDL_Window> window;
+std::shared_ptr<SDL_Renderer> renderer;
 
-	std::shared_ptr<SDL_Window> window;
-	std::shared_ptr<SDL_Renderer> renderer;
-
+void create_window() {
 	window = std::shared_ptr<SDL_Window>(
-			SDL_CreateWindow(
-					"SDL Window", /*название*/
+			SDL_CreateWindow("SDL Window", /*название*/
 					SDL_WINDOWPOS_CENTERED,
 					SDL_WINDOWPOS_CENTERED, /*расположение верхнего левого угла*/
 					WINDOW_WIDTH,
 					WINDOW_HEIGHT,
-					SDL_WINDOW_SHOWN /*показ*/),
+					SDL_WINDOW_SHOWN),
 			SDL_DestroyWindow);
-
-	if(window==nullptr){//окно не создалось
-		std::cerr << "Ќе могу создать окно: " << SDL_GetError() << std::endl;
-		return 1;
+	if (window == nullptr) { //окно не создалось
+		std::cerr <<
+				"Ќе могу создать окно: " <<
+				SDL_GetError() << std::endl;
+		exit(1);
 	}
+}
 
+void create_renderer() {
 	//окно успешно создалось
 	renderer = std::shared_ptr<SDL_Renderer>(
 			SDL_CreateRenderer(
 					window.get(), -1,
 					SDL_RENDERER_ACCELERATED),
 			SDL_DestroyRenderer);
-
-	if (renderer == nullptr) {//рендерер не создалс€
-		std::cerr << "Ќе могу создать рендерер: " << SDL_GetError()
-				<< std::endl;
-		return 1;
+	if (renderer == nullptr) { //рендерер не создалс€
+		std::cerr <<
+				"Ќе могу создать рендерер: " <<
+				SDL_GetError() << std::endl;
+		exit(1);
 	}
+}
 
-
-	if (renderer != nullptr) {
-		SDL_SetRenderDrawColor(renderer.get(), 31, 63, 95, 255);
-		SDL_RenderClear(renderer.get());
-
-		SDL_SetRenderDrawColor(renderer.get(), 255, 255, 255, 255);
-		for (int i = 0; i < 50; i++) {
-			SDL_RenderDrawLine(renderer.get(), 100 + i * 8, 100, 100, 500 - i * 8);
-			SDL_RenderDrawLine(renderer.get(), 500 - i * 8, 500, 500, 100 + i * 8);
-		}
-
-		SDL_RenderPresent(renderer.get());
-		SDL_Delay(10'000); /*задержка окна*/
-		SDL_DestroyRenderer(renderer.get());
+void render() {
+	SDL_SetRenderDrawColor(renderer.get(), 31, 63, 95, 255);
+	SDL_RenderClear(renderer.get());
+	SDL_SetRenderDrawColor(renderer.get(), 255, 255, 255, 255);
+	for (int i = 0; i <= 50; i++) {
+		SDL_RenderDrawLine(renderer.get(),
+				100 + i * 8, 100,
+				100, 500 - i * 8);
+		SDL_RenderDrawLine(renderer.get(),
+				500 - i * 8, 500,
+				500, 100 + i * 8);
 	}
+}
+
+int main(int, char**) {
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	create_window();
+	create_renderer();
+
+	render();
+
+	SDL_RenderPresent(renderer.get());
+	SDL_Delay(3'000); /*задержка окна*/
+	SDL_DestroyRenderer(renderer.get());
 
 	return 0;
 }
