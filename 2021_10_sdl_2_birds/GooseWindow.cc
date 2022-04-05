b4249383ff5b72045d4f0c5195eb74e79de48921
app-pro <app-pro@app-pro-pc>
2022-04-06 01:25:00 +0500

    Adding eagle, background and sun

--- 2021_10_sdl_2_birds/GooseWindow.cc
+++ 2021_10_sdl_2_birds/GooseWindow.cc
@@ -3,11 +3,18 @@
 
 GooseWindow::GooseWindow()
 : _goose(_renderer, "goose.png"),
-  _cloud(_renderer, "cloud.png")
+  _cloud(_renderer, "cloud.png"),
+  _eagle(_renderer, "eagle.png"),
+  _bg(_renderer, "bg.png"),
+  _sun(_renderer, "sun.png")
 {
 	SDL_SetWindowTitle(_window.get(), "Some evil birds window o_O");
 	for (unsigned i = 0; i < 5; i++)
 		phase[i] = 0.0;
+	eagle_x = 50.0;
+	eagle_y = 300.0;
+	sun_x = 0;
+	sun_y = 50;
 }
 
 void GooseWindow::render()
@@ -15,15 +22,22 @@ void GooseWindow::render()
 	//небо
 	SDL_SetRenderDrawColor(_renderer.get(), 63, 192, 255, 255);
 	SDL_RenderClear(_renderer.get());
+	//солнце
+	_sun.draw(sun_x, sun_y);
+	//фон
+	_bg.draw(0, 0);
 	//облака вдали
 	for (unsigned i = 0; i < 3; i++)
 		_cloud.draw(clouds[i]);
-	//гуси
+	//гусь
 	//_goose.draw(x, y, phase*20);
 	//_goose.draw(x, y, (sin(phase) + cos(phase)) / 2 * 180 / 3.1415);
+	//гуси
 	for (unsigned i = 0; i < 5; i++) {
 		_goose.draw(geese[i].x, geese[i].y, (sin(phase[i]) + cos(phase[i])) / 2 * 180 / 3.1415);
 	}
+	//орёл
+	_eagle.draw(eagle_x, eagle_y);
 	//облака вблизи
 	for (unsigned i = 3; i < 6; i++)
 			_cloud.draw(clouds[i]);
@@ -31,18 +45,22 @@ void GooseWindow::render()
 
 void GooseWindow::handle_keys(const Uint8 *keys)
 {
-	/*if (keys[SDL_SCANCODE_RIGHT])
-		x += 4;
-	if (keys[SDL_SCANCODE_LEFT])
-		x -= 4;
-	if (keys[SDL_SCANCODE_DOWN])
-		y += 4;
-	if (keys[SDL_SCANCODE_UP])
-		y -= 4;*/
+	if (eagle_x < width() - _eagle.width() and keys[SDL_SCANCODE_RIGHT])
+		eagle_x += 4;
+	if (eagle_x > 0 and keys[SDL_SCANCODE_LEFT])
+		eagle_x -= 4;
+	if (eagle_y < height() - _eagle.height() and keys[SDL_SCANCODE_DOWN])
+		eagle_y += 4;
+	if (eagle_y > 0 and keys[SDL_SCANCODE_UP])
+		eagle_y -= 4;
 }
 
 void GooseWindow::update()
 {
+	sun_x += 1.0;
+	if (sun_x >= width())
+		sun_x = 10.0;
+
 	auto pi = acos(-1.0);
 	for (unsigned i = 0; i < 5; i++) {
 		phase[i] += pi * (i - 0.6) / 90.0;
--- 2021_10_sdl_2_birds/GooseWindow.h
+++ 2021_10_sdl_2_birds/GooseWindow.h
@@ -9,6 +9,9 @@ class GooseWindow final: public Window
 private:
 	Texture _goose;
 	Texture _cloud;
+	Texture _eagle;
+	Texture _bg;
+	Texture _sun;
 
 	double phase[5];
 
@@ -28,6 +31,9 @@ private:
 			{420.0, 179.0, 480.0, 200.0},
 			{220.0, 230.0, 500.0, 300.0},
 	};
+
+	double eagle_x, eagle_y;
+	double sun_x, sun_y;
 
 public:
 	GooseWindow();
