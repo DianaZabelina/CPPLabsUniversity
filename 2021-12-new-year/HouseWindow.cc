18af869e54e951de8b499118f560bcb630fb07f8
app-pro <app-pro@app-pro-pc>
2022-04-18 07:48:19 +0500

    Adding sky, spruce and snowman

--- 2021-12-new-year/HouseWindow.cc
+++ 2021-12-new-year/HouseWindow.cc
@@ -8,19 +8,123 @@ HouseWindow::HouseWindow()
 }
 
 void HouseWindow::update() {
-	_angle += 2.0;
+	_angle += 0.5;
 }
 
 void HouseWindow::render() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
-	gluLookAt(	6.0, 6.0, 4.5,
+	gluLookAt(	4.0, 4.0, 4.5,
 				0.0, 0.0, 1.5,
 				0.0, 0.0, 1.0);
 	glRotated(_angle, 0.0, 0.0, 1.0);
+	Texture::disable();
+
+	/*
+	 * "Ќовогоднее настроение"
+	 *
+	 * - плоскость/крива€ поверхность
+	 * - в центре - Єлка, ствол - цилиндр, ветки - несколько конусов
+	 * вокруг Єлки - несколько снеговиков:  3 сферы,
+	 * 										нос - морковка (конус),
+	 * 										руки - цилиндры,
+	 * 										глаза - сферы и т.п.
+	 */
+
+	//_house_texture.bind();
+	//_house_model.draw();
+
+	//glDisable(GL_LIGHTING);
+	//glDisable(GL_FOG);
+	//glEnable(GL_LIGHTING);
+	//glEnable(GL_FOG);
+
+	glDisable(GL_LIGHTING);
+
 	Texture::enable();
-	_house_texture.bind();
-	_house_model.draw();
+	_sky_texture.bind();
+	draw_sphere(32, 16, 20.0);
+
+	glEnable(GL_LIGHTING);
+
+	glPopMatrix();
+
+	Texture::disable();
+
+	//поверхность
+	glBegin(GL_QUADS);
+	glNormal3d(0.0, 0.0, 1.0);
+	for (int i = -50; i < 50; i++)
+		for (int j = -50; j < 50; j++) {
+			glVertex3d(i + 0, j + 0, 0.0);
+			glVertex3d(i + 1, j + 0, 0.0);
+			glVertex3d(i + 1, j + 1, 0.0);
+			glVertex3d(i + 0, j + 1, 0.0);
+		}
+	//Єлка
+	glPushMatrix();
+	glTranslated(10.0, 0.0, 0.75);
+	Texture::enable();
+	//ствол
+	_tree_texture.bind();
+	draw_cylinder(12, 0.25, 1.5);
+	glTranslated(0.0, 0.0, 1.25);
+	//ветки
+	_spruce_texture.bind();
+	draw_cone(12, 1, 1.5);
+	glTranslated(0.0, 0.0, 1.0);
+	draw_cone(12, 0.75, 1.5);
+	glPopMatrix();
+	Texture::disable();
+
+
+	//снеговик
+	glPushMatrix();
+	glTranslated(10.0, 0.0, -2.0);
+	draw_sphere(16, 10, 1.0);
+	glTranslated(0.0, 0.0, 1.25);
+	draw_sphere(12, 8, 0.7);
+	glTranslated(0.0, 0.0, 0.9);
+	draw_sphere(10, 6, 0.4);
+	//нос
+	Texture::enable();
+	_carrot_texture.bind();
+	glRotated(90.0, 1.0, 1.0, 0.0);
+	draw_cone(8, 0.1, 1.0);
+	//руки
+	_tree_texture.bind();
+	glTranslated(0.05, 0.0, -0.05);
+	glRotated(90.0, 1.0, 0.0, 0.0);
+	draw_cylinder(8, 0.05, 1.75);
+	glTranslated(0.05, 0.0, 0.05);
+	glRotated(90.0, 0.0, 1.0, 0.0);
+	draw_cylinder(8, 0.05, 1.75);
+	//глаза
+	_rock_texture.bind();
+	glTranslated(0.25, 0.35, 0.0);
+	draw_sphere(10, 6, 0.05);
+	glTranslated(-0.3, 0.0, -0.3);
+	draw_sphere(10, 6, 0.05);
+	glTranslated(-0.4, 0.25, 0.5);
+	draw_sphere(10, 6, 0.1);
+	glTranslated(-0.3, 0.075, 0.3);
+	draw_sphere(10, 6, 0.1);
+	glPopMatrix();
+	Texture::disable();
+
+	/*
+	 * draw_prism(24, 1, 2);
+	 * _cylinder_texture.bind();
+	 * draw_cylinder(24, 1, 2);
+	 * draw_pyramid(16, 1, 2);
+	 * _cone_texture.bind();
+	 * draw_cone(12, 1, 2);
+	 * draw_disco_ball(16, 8, 2.0);
+	 * _earth_texture.bind();
+	 * _moon_texture.bind();
+	 * _sun_texture.bind();
+	 * draw_sphere(24, 12, 2.0);
+	 */
 }
 
 void HouseWindow::setupGL() {
@@ -33,6 +137,6 @@ void HouseWindow::setupGL() {
 
 	double aspect_ratio = double(width()) / double(height());
 	glMatrixMode(GL_PROJECTION);
-	gluPerspective(45.0, aspect_ratio, 0.1, 20.0);
+	gluPerspective(45.0, aspect_ratio, 0.1, 40.0);
 	glMatrixMode(GL_MODELVIEW);
 }
--- 2021-12-new-year/HouseWindow.h
+++ 2021-12-new-year/HouseWindow.h
@@ -4,11 +4,26 @@
 #include "Window.h"
 #include "Model.h"
 #include "Texture.h"
+#include "Primitives.h"
 
 class HouseWindow: public Window {
 private:
 	Model _house_model;
 	Texture _house_texture { "House.png" };
+	Texture _sky_texture { "Stars.jpg" };
+	Texture _tree_texture { "Tree.png" };
+	Texture _spruce_texture { "Spruce.png" };
+	Texture _carrot_texture { "Carrot.png" };
+	Texture _rock_texture { "Rock.png" };
+
+	/*
+	 * Texture _cylinder_texture { "Cylinder.png" };
+	 * Texture _cone_texture { "Cone.png" };
+	 * Texture _earth_texture { "Earth.jpg" };
+	 * Texture _moon_texture { "Moon.jpg" };
+	 * Texture _sun_texture { "Sun.jpg" };
+	 */
+
 	double _angle = 0.0;
 public:
 	static constexpr int WINDOW_WIDTH = 1280;
